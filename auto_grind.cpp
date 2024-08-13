e16efd0fcea702015c0b4677cc368d3246b998ef
x64dot <142705403+x64dot@users.noreply.github.com>
2024-08-12 23:58:13 -0400

    Update auto_grind.cpp
    Some improvements.

--- auto_grind.cpp
+++ auto_grind.cpp
@@ -4,20 +4,43 @@
 #include <chrono>
 #include <thread>
 
-int main(){
+int main(int argc, char** argv){
   bool setup_track = true;
   int count = 0;
   std::string command1 = "xdotool type !work";
   std::string command2 = "xdotool type \"!dep all\"";
   std::string command3 = "xdotool key Return";
+  
+  if (argc >= 2){
+     std::string check(argv[1]);
+     if (check == "--low" || check == "-l"){
+         setup_track = false;
+     }
+     else if (check == "--help" || check == "-h"){
+              std::cout << "-h/--help to show this help message." << std::endl;
+              std::cout << "-l/--low to low the wait time to 10 seconds instead of 30 seconds to start the auto type process." << std::endl;
+              return 0;
+     }
+     else{
+          std::cout << "[!] Invalid arguments, do -h/--help for more information" << std::endl;
+          return 0;
+     }    
+  }            
 
   if (setup_track){
      std::cout << "[+] You have 30 seconds to locate where you gonna autotype" << std::endl;
      std::this_thread::sleep_for(std::chrono::seconds(30));
      setup_track = false;
   }
+
+  else{
+       std::cout << "[-] You have 10 seconds to locate where you gonna autotype" << std::endl;
+       std::this_thread::sleep_for(std::chrono::seconds(10));
+  }
+       
   std::cout << "[+] The auto typing process starts now!" << std::endl;
   std::cout << "[+] You can do ctrl+c to stop the program." << std::endl;
+
   while (true){
       if (count != 10){
          int temp1 =  system(command1.c_str());
