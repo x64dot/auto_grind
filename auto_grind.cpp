dbfb504bbc717a9fb6946d8791793ec9f9ad6eb0
x64dot <142705403+x64dot@users.noreply.github.com>
2024-08-12 22:47:32 -0400

    Create auto_grind.cpp

---
+++ auto_grind.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <chrono>
+#include <thread>
+
+int main(){
+  bool setup_track = true;
+  int count = 0;
+  std::string command1 = "xdotool type !work";
+  std::string command2 = "xdotool type \"!dep all\"";
+  std::string command3 = "xdotool key Return";
+
+  if (setup_track){
+     std::cout << "[+] You have 30 seconds to locate where you gonna autotype" << std::endl;
+     std::this_thread::sleep_for(std::chrono::seconds(30));
+     setup_track = false;
+  }
+  std::cout << "[+] The auto typing process starts now!" << std::endl;
+  std::cout << "[+] You can do ctrl+c to stop the program." << std::endl;
+  while (true){
+      if (count != 10){
+         int temp1 =  system(command1.c_str());
+         if (temp1 != 0){std::cout << "[!] Error executing command! Command: " << command1 << std::endl;}
+         int temp2 = system(command3.c_str());
+         if (temp2 != 0){std::cout << "[!] Error executing command! Command: " << command3 << std::endl;}
+         count++;
+      }
+      else{
+           int temp1 = system(command2.c_str());
+           if (temp1 != 0){std::cout << "[!] Error executing command! Command: " << command2 << std::endl;}
+           int temp2 = system(command3.c_str());
+           if (temp2 != 0){std::cout << "[!] Error executing command! Command: " << command3 << std::endl;}
+           count = 0;
+      }
+      std::this_thread::sleep_for(std::chrono::minutes(1));
+  }
+
+  return 0;
+}
